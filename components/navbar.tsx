94f6ebcd18acdbc425e7efdea7b57e7b0edba521
aeggydev <kucernak.a@gmail.com>
2022-01-17 20:19:21 +0100

    Add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,22 @@
+interface LinkProps {
+    link: string
+    text: string
+}
+
+function LinkLeft({link, text}: LinkProps) {
+    return <a className="" href={link}>{text}</a>
+}
+
 export default function Navbar() {
-    return <div>foobar</div>
+    return <div className="bg-[#2D2D2D] h-16 w-full flex justify-between items-center px-16 text-white text-xl font-semibold absolute">
+        <div className="flex gap-16">
+            <LinkLeft link="/home" text="Domov" />
+            <LinkLeft link="/coaches" text="Coachové" />
+            <LinkLeft link="/services" text="Služby" />
+            <LinkLeft link="/reviews" text="Recenze" />
+        </div>
+        <div className="bg-[#F65A5A] h-10 flex items-center rounded-sm px-5">
+            <a href="/404" className="bg-[#F65A5A]">Kontaktovat</a>
+        </div>
+    </div>
 }
