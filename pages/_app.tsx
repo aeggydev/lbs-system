bab19ffb4fd718c4071c5a8607aac86f31478d65
aeggydev <kucernak.a@gmail.com>
2022-01-18 21:16:13 +0100

    Add font Chakra Petch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
