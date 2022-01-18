3e25ff8bd024018156635740fd1b502c6f0856b1
aeggydev <kucernak.a@gmail.com>
2022-01-18 20:18:22 +0100

    Fix sizing index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/navbar'
 
 const Home: NextPage = () => {
   return (
-    <div>
-            <div className="h-[100vh] bg-red-400">
+    <div className='w-full min-h-full bg-red-400'>
+            <div>
               <div>
                 <div>LBs System</div>
                 <div>Profesionální coaching</div>
