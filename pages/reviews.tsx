b656a27b0aa83b13470229612a2416b7388c4683
aeggydev <kucernak.a@gmail.com>
2022-01-18 20:44:45 +0100

    Make reviews page better

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -3,23 +3,33 @@ import Image from "next/image";
 import { Review, data } from "../data/reviews";
 
 interface ReviewProps {
-  review: Review
-  index: number
+  review: Review;
+  index: number;
 }
 
 function Review({ review, index }: ReviewProps) {
+  const url = `/review/${index}`;
+
+  const Img = (
+    <Image
+      alt="Fotka recenzenta"
+      src={review.image}
+      width={2048}
+      height={1365}
+      className="rounded-md"
+    />
+  );
+
   return (
     <div className="text-white font-serif">
-      <Image
-        alt="Fotka recenzenta"
-        src={review.image}
-        width={2048}
-        height={1365}
-        className="rounded-md"
-      />
-      <div className="py-2">{review.title}</div>
+      {review.textLong ? <a href={url}>{Img}</a> : Img}
+      <div className="py-2 text-xl">{review.title}</div>
       <div className="italic">“{review.text}”</div>
-      <a href={`/review/${index}`}>CLICK me</a>
+      {review.textLong ? (
+        <a href={url} className="font-bold pl-4 pt-2 block text-lg">
+          Přečíst více. →
+        </a>
+      ) : null}
     </div>
   );
 }
@@ -27,8 +37,8 @@ function Review({ review, index }: ReviewProps) {
 export default function Reviews() {
   return (
     <div className="bg-[#3D3D3D] min-h-screen">
-      <div className="text-center text-white text-3xl">Recenze</div>
-      <div className="grid grid-cols-2 p-16 gap-8">
+      <div className="text-center text-white text-3xl mt-4">Recenze</div>
+      <div className="grid grid-cols-2 py-10 px-40 gap-8">
         {data.map((x, i) => (
           <Review review={x} key={i} index={i} />
         ))}
