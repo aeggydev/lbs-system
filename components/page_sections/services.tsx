37db1c39e50605f11b25d1ce276977c40caa2071
aeggydev <kucernak.a@gmail.com>
2022-01-25 22:33:34 +0100

    Put spacing between paragraphs in first services part

--- components/page_sections/services.tsx
+++ components/page_sections/services.tsx
@@ -3,21 +3,30 @@ export default function Services() {
     <div className="py-12 px-44 flex flex-col gap-5" id="services">
       <div className="text-3xl font-semibold text-center">Služby</div>
       <div>
-        <div className="font-medium text-xl text-center pb-2">LBs system full time coaching</div>
-        <div>
-          Tento balíček obsahuje spoustu produktů. Mezi hlavní se dá počítat
-          strukturovaný tréninkový program uzpůsobený na daného jedince, pro
-          danou chvíli a s daným momentálním či dlouhodobým cílem. Nepřetržitou
-          komunikaci, jak v podobě hovorů, tak zpráv. Zpětná vazba v týdenní
-          periodě. A to buď formou videa které obsahuje zhodnocení celého týdne,
-          představení nového týdne, různých připomínek, rad k technice a spoustu
-          užitečných informací nebo dohodnutého videohovoru. Jako jedno z
-          dalších hlavních produktů, se dá vnímat to nejdůležitější: naše
-          know-how. A to nejen z hlediska programingu, ale také z hlediska
-          mealprepu, mindset, weight cutting, pomůžeme vám vyřešit a zodpovědět
-          všechny otázky, co se týče závodů, přihlášek na závody a to nejen
-          národní ale také mezinárodní, se znalostí pravidel. Zkrátka se vším co
-          vás napadne, a nebudete si vědět rady.
+        <div className="font-medium text-xl text-center pb-2">
+          LBs system full time coaching
+        </div>
+        <div className="flex flex-col gap-4">
+          <p>
+            Tento balíček obsahuje spoustu produktů. Mezi hlavní se dá počítat
+            strukturovaný tréninkový program uzpůsobený na daného jedince, pro
+            danou chvíli a s daným momentálním či dlouhodobým cílem.
+          </p>
+          <p>
+            Nepřetržitou komunikaci, jak v podobě hovorů, tak zpráv. Zpětná
+            vazba v týdenní periodě. A to buď formou videa které obsahuje
+            zhodnocení celého týdne, představení nového týdne, různých
+            připomínek, rad k technice a spoustu užitečných informací nebo
+            dohodnutého videohovoru.
+          </p>
+          <p>
+            Jako jedno z dalších hlavních produktů, se dá vnímat to
+            nejdůležitější: naše know-how. A to nejen z hlediska programingu,
+            ale také z hlediska mealprepu, mindset, weight cutting, pomůžeme vám
+            vyřešit a zodpovědět všechny otázky, co se týče závodů, přihlášek na
+            závody a to nejen národní ale také mezinárodní, se znalostí
+            pravidel. Zkrátka se vším co vás napadne, a nebudete si vědět rady.
+          </p>
         </div>
       </div>
       <div>
@@ -31,8 +40,8 @@ export default function Services() {
         </div>
       </div>
       <div>
-          <div className="font-medium text-xl text-center pb-2">LBs system</div>
-          <div>Popisek zatím není</div>
+        <div className="font-medium text-xl text-center pb-2">LBs system</div>
+        <div>Popisek zatím není</div>
       </div>
     </div>
   );
