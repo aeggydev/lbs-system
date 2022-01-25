12f84280008c4f6f5af32213c1dacc81dafc4f44
aeggydev <kucernak.a@gmail.com>
2022-01-25 21:33:13 +0100

    Fix links in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,8 +13,8 @@ export default function Navbar() {
     return <div className="bg-[#2D2D2D] h-16 w-full flex justify-between items-center px-16 text-white text-xl font-semibold sticky top-0 z-50">
         <div className="flex gap-16">
             <LinkLeft link="/" text="Domov" />
-            <LinkLeft link="#coaches" text="Coachové" />
-            <LinkLeft link="#services" text="Služby" />
+            <LinkLeft link="/#coaches" text="Coachové" />
+            <LinkLeft link="/#services" text="Služby" />
             <LinkLeft link="/reviews" text="Recenze" />
         </div>
         <div className="bg-[#F65A5A] h-10 flex items-center rounded-sm px-5">
