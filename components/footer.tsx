337f6a719e23d86bc2f13852a7f2baecaaf7dba9
aeggydev <kucernak.a@gmail.com>
2022-01-25 20:51:04 +0100

    Create footer

---
+++ components/footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return <div className="h-16 bg-zinc-800 text-white flex flex-row place-content-between items-center px-4">
+        <div>© 2022 — <span className="font-semibold">LBs System</span></div>
+        <div></div>
+    </div>
+}
