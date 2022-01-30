ed8e17fe0b99b320ab76e9de971448195051a80a
aeggydev <kucernak.a@gmail.com>
2022-01-30 18:30:47 +0100

    Fix up carousel

--- components/page_sections/nas_team.tsx
+++ components/page_sections/nas_team.tsx
@@ -3,20 +3,31 @@ import { NasTeamProps } from "../../data/NasTeam";
 import FacebookIcon from "../icons/facebook";
 import InstagramIcon from "../icons/instagram";
 import YoutubeIcon from "../icons/youtube";
-import Image from "next/image"
+import Image from "next/image";
 
-
-function EmblaSlide(props: {image: StaticImageData, index: number}) {
+function EmblaSlide(props: { image: StaticImageData; index: number }) {
   const alt = `Fotka ${props.index + 1}`;
-  return <div className="embla__slide relative grid items-center" style={{flex: "0 0 100%"}}>
-    <Image src={props.image} alt={alt} />
-  </div>
+  return (
+    <div
+      className="embla__slide grid items-center"
+      style={{ flex: "0 0 100%" }}
+    >
+      <div className="embla__slide__inner">
+        <div className="embla__slide__img">
+          <Image
+            src={props.image}
+            alt={alt}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default function NasTeam(props: NasTeamProps) {
-  const [emblaRef] = useEmblaCarousel({loop: true})
-  
-  const maxHeight = Math.max(...props.images.map(x => x.height))
+  const [emblaRef] = useEmblaCarousel({ loop: true });
+
+  const maxHeight = Math.max(...props.images.map((x) => x.height));
 
   return (
     <div
@@ -26,7 +37,9 @@ export default function NasTeam(props: NasTeamProps) {
       <div className="rounded-t-md bg-zinc-400">
         <div className="embla overflow-hidden w-full h-full" ref={emblaRef}>
           <div className="embla__container flex h-full">
-            {props.images.map((x, i) => <EmblaSlide image={x} index={i} key={i} />)}
+            {props.images.map((x, i) => (
+              <EmblaSlide image={x} index={i} key={i} />
+            ))}
           </div>
         </div>
       </div>
