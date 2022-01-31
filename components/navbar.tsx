b8831624ece1174cceddee115a6577c2f99554a4
aeggydev <kucernak.a@gmail.com>
2022-01-31 17:53:17 +0100

    Make navbar look a bit better

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,7 +19,7 @@ function LinkLeft({ link, text }: LinkProps) {
 export default function Navbar() {
   return (
     <div className="bg-[#2D2D2D] h-16 min-w-full grid grid-cols-3 items-center px-4 md:px-16 text-white text-sm md:text-xl font-semibold sticky top-0 z-50">
-      <div className="flex gap-2 md:gap-16 col-start-1 col-end-2">
+      <div className="flex gap-3 md:gap-16 col-start-1 col-end-2">
         <LinkLeft link="/" text="Domov" />
         <LinkLeft link="/#coaches" text="Coachové" />
         <LinkLeft link="/#services" text="Služby" />
@@ -27,7 +27,7 @@ export default function Navbar() {
       </div>
       <div className="col-start-8">
         <div className="bg-[#F65A5A] h-10 grid rounded-sm px-2 md:px-5 place-content-center">
-          <Link href="/404">Kontaktovat</Link>
+          <Link href="/404">Kontakt</Link>
         </div>
       </div>
     </div>
