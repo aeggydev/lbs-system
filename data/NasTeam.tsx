a388c2c58b16acefdbd155510718d66c6992a552
aeggydev <kucernak.a@gmail.com>
2022-03-16 16:51:38 +0100

    Add photos of Jan

--- data/NasTeam.tsx
+++ data/NasTeam.tsx
@@ -1,7 +1,10 @@
-import one from "../public/NasTeam/1.jpg"
-import two from "../public/NasTeam/2.jpg"
-import three from "../public/NasTeam/3.jpg"
-import four from "../public/NasTeam/4.jpg"
+import davidOne from "../public/NasTeam/david1.jpg"
+import davidTwo from "../public/NasTeam/david2.jpg"
+import davidThree from "../public/NasTeam/david3.jpg"
+import davidFour from "../public/NasTeam/david4.jpg"
+import janOne from "../public/NasTeam/jan1.jpg"
+import janTwo from "../public/NasTeam/jan2.jpg"
+import janThree from "../public/NasTeam/jan3.jpg"
 
 export interface NasTeamProps {
   description: string;
@@ -17,13 +20,13 @@ export const TeamData: NasTeamProps[] = [
     description: `David závodí aktivně od roku 2017. Za jeho dlouholetou kariéru nasbíral mnoho zkušeností z jak národních tak i z mezinárodních soutěží, které rád předá dalším sportovcům.`,
     descFirst: false,
     bg: "#A35959",
-    images: [one, two, three, four]
+    images: [davidOne, davidTwo, davidThree, davidFour]
   },
   {
     name: "Jan Bolech",
     description: `Jeho trenérský cíl je zajistit svým klientům co nejlepší výkon v den závodů. Snaží se, aby každý sportovec, se kterým pracuje, získával sebevědomí každý trénink.`,
     descFirst: true,
     bg: "#5AAC53",
-    images: [three]
+    images: [janOne, janTwo, janThree]
   },
 ];
